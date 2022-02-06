8bc67afe700f6219aeb314b6924951f13a42cfb4
Akshit <akshitarora9211@gmail.com>
2022-02-06 13:25:26 +0530

    add font size as of design

--- components/Card.js
+++ components/Card.js
@@ -2,8 +2,9 @@ import { formatDate } from "../utils/formatDate";
 
 /* eslint-disable @next/next/no-img-element */
 function Card({ cardData }) {
+  console.log(cardData);
   return (
-    <div className='bg-[#232323] font-semibold rounded-2xl shadow-lg p-4 min-w-fit overflow-hidden duration-500 z-10 md:hover:scale-105'>
+    <div className='bg-[#232323] font-semibold rounded-2xl shadow-lg p-4 pb-0 min-w-[260px] overflow-hidden duration-500 z-10 md:hover:scale-105'>
       <div className='flex justify-between'>
         <img
           className='w-24 h-24 rounded-3xl shadow-lg'
@@ -11,18 +12,20 @@ function Card({ cardData }) {
           alt={cardData.description}
         />
         <div className='flex-1 ml-5'>
-          <h1 className='text-lg text-gray-200'>{cardData.prduct_name}</h1>
-          <h3 className='text-sm text-gray-400 '>{cardData.brand_name}</h3>
-          <p className='text-xs text-gray-400 mt-4'>$ {cardData.price}9</p>
+          <h1 className='text-[15px] text-gray-200'>{cardData.product_name}</h1>
+          <h3 className='text-[13px] text-gray-50/60 '>
+            {cardData.brand_name}
+          </h3>
+          <p className='text-[13px] text-gray-50 mt-2'>$ {cardData.price}9</p>
         </div>
       </div>
-      <div className='flex justify-around items-center mt-3 gap-3'>
-        <h1 className='text-md text-gray-500'>{cardData.address.city}</h1>
-        <h3 className='text-sm text-gray-400'>
+      <div className='flex justify-between items-center mt-3'>
+        <h1 className='text-[13px] text-gray-500'>{cardData.address.city}</h1>
+        <h3 className='text-[13px] font-semibold text-gray-400'>
           Date: {formatDate(cardData.date)}
         </h3>
       </div>
-      <p className='text-xs text-gray-400 mt-4'>{cardData.discription}</p>
+      <p className='text-[11px] text-gray-400 mt-2'>{cardData.discription}</p>
     </div>
   );
 }

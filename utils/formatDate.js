5c67cbac7a8895a97f7e6fcd22c246703bbdf8f8
Akshit <akshitarora9211@gmail.com>
2022-02-06 13:23:36 +0530

    fix month value

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -3,7 +3,9 @@ export function formatDate(date) {
   const dateVal =
     _date.getDate() < 10 ? "0" + _date.getDate() : _date.getDate();
   const month =
-    _date.getMonth() < 10 ? "0" + _date.getMonth() : _date.getMonth();
+    _date.getMonth() + 1 < 10
+      ? "0" + (_date.getMonth() + 1)
+      : _date.getMonth() + 1;
   const year = _date.getFullYear();
   return `${dateVal}:${month}:${year}`;
 }
